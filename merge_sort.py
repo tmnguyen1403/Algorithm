fe96f27ff929801ce6601ead5733d43455c1c263
Minh Nguyen <tmnguyen1403@gmail.com>
2020-12-30 19:53:02 -0600

    improved merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,5 +1,7 @@
 class MergeSort:
-    #divide array by half, sort both partitions, then merge
+    def __init__(self):
+        self.CUTOFF = 10
+
     def merge(self, arr, lo, mid, hi):
         #precondition: arr[lo..mid] is sorted
         #precondition: arr[mid+1..high] is sorted
@@ -20,12 +22,28 @@ class MergeSort:
                 arr[index] = aux[right]
                 right += 1
 
+    #Use insertionSort for small size array 10 ~ 20 items
+    def insertionSort(self, arr, lo, hi):
+        for index in range(lo + 1, hi + 1):
+            pivot = index
+            while pivot > lo and arr[pivot] < arr[pivot - 1]:
+                tmp = arr[pivot]
+                arr[pivot] = arr[pivot - 1]
+                arr[pivot - 1] = tmp
+                pivot -= 1
+
     def performSort(self, arr,lo, hi):
         if lo >= hi:
             return
+        if (hi <= lo + self.CUTOFF - 1):
+            self.insertionSort(arr, lo, hi)
+
         mid = int(lo + (hi - lo)/2)
         self.performSort(arr, lo, mid)
         self.performSort(arr, mid + 1, hi)
+        #will not merge if already sorted
+        if (arr[mid + 1] > arr[mid]):
+            return
         self.merge(arr, lo, mid, hi)
 
     def sort(self, arr):
@@ -33,7 +51,7 @@ class MergeSort:
 
         return
 
-data = [5,6,9,10,12,7,-1,5,5,5,8,9,10]
+data = [5,6,9,10,12,7,-1,5,5,5,8,9,10,5,6,9,10,12,7,-1,5,5,5,8,9,10,5,6,9,10,12,7,-1,5,5,5,8,9,10]
 ob = MergeSort()
 print(data)
 ob.sort(data)
